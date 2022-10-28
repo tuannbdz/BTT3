2ca0f1e1e33b0c98cd751541339ace32d90700eb
Tuan Nguyen Bao <minecraftqn123@gmail.com>
2022-10-28 09:01:15 +0700

    Add project files.

---
+++ MyString.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class MyString
+{
+	char* c;
+	int n;
+public:
+	int length();
+	char* subString(int, int);
+	char* insert(int, char*);
+	char* operator+(const MyString&);
+	friend istream& operator>>(istream&, MyString&);
+	friend ostream& operator<<(ostream&, MyString&);
+};
+
