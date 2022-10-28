af3df14ccb25b17f570a9667a1877432d28907d3
Gia Bẻo <baog3083@gmail.com>
2022-10-28 09:21:00 +0700

    bao

--- MyString.cpp
+++ MyString.cpp
@@ -1,1 +1,14 @@
 #include "MyString.h"
+
+int MyString::length() {
+	return n;
+}
+
+char* MyString::subString(int pos, int num) {
+	char* sub = new char[num + 1];
+	for (int i = 0; i < num; i++) {
+		sub[i] = c[pos + i];
+	}
+	sub[num] = '\0';
+	return sub;
+}
